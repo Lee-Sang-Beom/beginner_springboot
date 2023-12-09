2dc1e3ca7684edb046a9cff5e151f05b4d5bef9c
이상범 <tkdqja975@naver.com>
2023-12-09 18:49:18 +0900

    실행오류 수정 / main func 추가

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,27 +1,29 @@
 package hello.hellospring.controller;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import hello.hellospring.HelloSpringApplication;
 
 @Controller
 public class HelloController {
 
+    public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+    
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","hello spring!");
         return "hello";
     }
 
-    // 추가: MVC
     @GetMapping("hello-mvc")
-
-    // 외부에서 파라미터를 받으려면, @RequestParams 사용 
     public String helloMvc(@RequestParam("name") String name, Model model){
-        // 파라미터로 넘어온 name을 model에 "name"이라는 key로 전달
         model.addAttribute("name", name);
-
-        // resources/templates/[return String]와 연결
         return "hello-template";
     }
 }
