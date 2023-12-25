03fc4dc0a9c91bd5f2c6008af3c91edeef725cc8
이상범 <tkdqja975@naver.com>
2023-12-25 13:13:25 +0900

    회원 도메인과 리포지토리 정의하기 학습

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import java.util.List;
+import java.util.Optional;
+
+// 회원 리포지토리 인터페이스: 회원 객체를 저장하는 저장소 
+public interface MemberRepository {
+
+    // 회원 저장: 저장 시, 저장한 회원정보 반환
+    Member save(Member member);
+
+    // 아이디로 회원 찾는 걸 만듬
+    // (Optional: findById로 유저를 못찾고 null이 반환될 수 있음. Optional이라는 것으로 null을 감싸는 방법임)
+    Optional<Member> findById(Long id);
+
+    // 이름으로 회원 찾는 걸 만듬
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+
+}
